d1f3fcc4f84360b90d751c6628e34fb2e79f6179
kaikaikai <151206021+kaireidie@users.noreply.github.com>
2026-08-26 02:20:31 +0300

    feat(Front/Bff) Added simple bff, updated compose.yaml. updated README.md, added Android simple, fix CI/Cd
    * feat(android): add Kotlin Android application
    
    * feat(ignore) Stop tracking local.properties
    
    * feat(CI/CD) Added Android sdk to test
    
    * feat(CI/CD) Fix gitlab ci/cd error
    
    * feat(CI/CD) Fix gitlab ci/cd error #2
    
    * feat(CI/CD) Fix github ci/cd error
    
    * feat(CI/CD) Fix github ci/cd error #2
    
    * feat(CI/CD) Fix github ci/cd error #3
    
    * feat(CI/CD) Fix github ci/cd error #4
    
    * feat(CI/CD) Fix gitlab ci/cd error #1
    
    * feat(CI/CD) Fix github ci/cd error #228
    
    * feat(Front/Bff) Added simple bff, updated compose.yaml. updated README.md

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,1 +1,25 @@
+pluginManagement {
+    repositories {
+        google()
+        mavenCentral()
+        gradlePluginPortal()
+    }
+}
+
+dependencyResolutionManagement {
+    repositoriesMode.set(RepositoriesMode.FAIL_ON_PROJECT_REPOS)
+    repositories {
+        google()
+        mavenCentral()
+    }
+}
+
+plugins {
+    id("org.jetbrains.kotlin.jvm") version "2.4.10" apply false
+    id("com.android.application") version "9.1.1" apply false
+    id("org.jetbrains.kotlin.plugin.compose") version "2.4.10" apply false
+}
+
 rootProject.name = "logistic-platform"
+
+include(":android:app")
